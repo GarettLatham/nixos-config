667721b169643a20bffd2bb5032ea1950715e8bf
System administrator <github.crewman140@passmail.com>
2025-09-24 19:50:14 -0400

    Fix flake.nix syntax; 25.05 inputs/outputs

--- flake.nix
+++ flake.nix
@@ -1,25 +1,39 @@
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+{
+  description = "NixOS + Home-Manager + nixvim (25.05)";
 
-  home-manager.url = "github:nix-community/home-manager/release-25.05";
-  home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-  nixvim.url = "github:nix-community/nixvim";
-  nixvim.inputs.nixpkgs.follows = "nixpkgs";
-};
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-# ...
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-modules = [
-  ./configuration.nix
-  home-manager.nixosModules.home-manager
+  outputs = { self, nixpkgs, home-manager, nixvim, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        # Your normal system config:
+        ./configuration.nix
 
-  # Use the new output name on 25.05:
-  { home-manager.sharedModules = [ nixvim.homeModules.nixvim ]; }
+        # Enable Home-Manager as a NixOS module:
+        home-manager.nixosModules.home-manager
 
-  {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.users.user1 = import ./home/users/user1/home.nix;
-  }
-];
+        # Expose nixvim's Home-Manager module (25.05 uses 'homeModules'):
+        { home-manager.sharedModules = [ nixvim.homeModules.nixvim ]; }
+
+        # Attach your user's Home-Manager config:
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.user1 = import ./home/users/user1/home.nix;
+        }
+      ];
+    };
+  };
+}
