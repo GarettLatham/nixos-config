06660e4b04e40e80daa256a12dd8314a9951eb24
System administrator <github.crewman140@passmail.com>
2025-09-24 22:22:10 -0400

    overlay: fix syntax; guard dockerfile-language-server lookup

--- overlays/dockerls.nix
+++ overlays/dockerls.nix
@@ -1,8 +1,12 @@
-# Overlay that provides pkgs.dockerfile-language-server
-self: super: {
-  dockerfile-language-server =
-    # prefer the normal nodePackages if present
-    (super.nodePackages.dockerfile-language-server-nodejs or null)
-    # fall back to nodePackages_latest on channels that use it
-    or (super.nodePackages_latest.dockerfile-language-server-nodejs or null);
+# Provide pkgs.dockerfile-language-server for nixvim docs
+self: super:
+let
+  np  = if super ? nodePackages        then super.nodePackages        else {};
+  npl = if super ? nodePackages_latest then super.nodePackages_latest else {};
+  dls =
+    if np  ? dockerfile-language-server-nodejs then np.dockerfile-language-server-nodejs
+    else if npl ? dockerfile-language-server-nodejs then npl.dockerfile-language-server-nodejs
+    else null;
+in {
+  dockerfile-language-server = dls;
 }
