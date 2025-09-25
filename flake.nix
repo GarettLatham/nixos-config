3063500a963ee00b1ac04680b501692351482662
System administrator <github.crewman140@passmail.com>
2025-09-25 10:43:03 -0400

    HM: enable automatic backups for file collisions

--- flake.nix
+++ flake.nix
@@ -26,6 +26,8 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.user1 = import ./home/users/user1/home.nix;
+          # NEW: auto-backup any pre-existing files HM would overwrite
+          home-manager.backupFileExtension = "hm-bak"
         }
       ];
     };
